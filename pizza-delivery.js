2696da0ed7ce8cd68bab46187b63db264a17b38a
Samantha <sleahsolomon@gmail.com>
2021-11-16 10:18:58 -0500

    refactor part 1 with helper

--- pizza-delivery.js
+++ pizza-delivery.js
@@ -2,6 +2,27 @@ const lo = require('lodash/lang')
 const fs = require('fs')
 const input = fs.readFileSync('input.txt', 'utf-8')
 
+function createGird (array, name, grid){
+  if(array === '^'){
+    name.y+=1
+  } else if (array === 'v'){
+    name.y-=1
+  } else if (array === '>'){
+    name.x+=1
+  } else {
+    name.x-=1
+  }
+  let alreadyVisited = false
+  for (const house of grid) {
+    if(lo.isEqual(house, name)){
+      alreadyVisited = true
+      break
+    }
+  }
+  if (!alreadyVisited){
+  grid.push(structuredClone(name))
+  }
+}
 
 //* Part 1
 
@@ -11,25 +32,7 @@ function part1 (directions){
   let arrowArray = directions.split('')
 
   for (let i = 0; i < arrowArray.length; i++) {
-    if(arrowArray[i] === '^'){
-      mariaPosition.y+=1
-    } else if (arrowArray[i] === 'v'){
-      mariaPosition.y-=1
-    } else if (arrowArray[i] === '>'){
-      mariaPosition.x+=1
-    } else {
-      mariaPosition.x-=1
-    }
-    let alreadyVisited = false
-    for (const house of grid) {
-      if(lo.isEqual(house, mariaPosition)){
-        alreadyVisited = true
-        break
-      }
-    }
-    if (!alreadyVisited){
-    grid.push(structuredClone(mariaPosition))
-    }
+    createGird(arrowArray[i], mariaPosition, grid)
   }
   return grid.length
 }
@@ -38,30 +41,8 @@ console.log("Part 1:", part1(input))
 
 
 
-
 //* Part 2
 
-function createGird (array, name, grid){
-    if(array === '^'){
-      name.y+=1
-    } else if (array === 'v'){
-      name.y-=1
-    } else if (array === '>'){
-      name.x+=1
-    } else {
-      name.x-=1
-    }
-    let alreadyVisited = false
-    for (const house of grid) {
-      if(lo.isEqual(house, name)){
-        alreadyVisited = true
-        break
-      }
-    }
-    if (!alreadyVisited){
-    grid.push(structuredClone(name))
-    }
-  }
 
 function part2 (directions){
   let grid = [ {x: 0, y: 0} ]
